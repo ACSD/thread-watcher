133cbdcd58da4f593db0aa42455212d6a4110b48
ACSD <dev.acsd@gmail.com>
2020-02-01 15:07:32 -0800

    begin heroku compat

--- index.js
+++ index.js
@@ -31,11 +31,4 @@ client.on('message', async (message) => {
 
 client.login(process.env.DISCORD_TOKEN);
 
-var http=require('http')
-var server=http.createServer((function(request,response)
-{
-	response.writeHead(200,
-	{"Content-Type" : "text/plain"});
-	response.end("Hello World\n");
-}));
-server.listen(process.env.PORT);
+require('./src/server');
